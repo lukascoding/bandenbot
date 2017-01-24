4f357ca938901f4dd6b782e46cbd902516774b0f
Lukas <lukasm@my.com>
2017-01-24 09:48:21 +0100

    Added config.json and Config-Class for loading the config-file

---
+++ bandenbot/src/bandenbot/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace bandenbot
+{
+    public class Config
+    {
+        private const string ConfigFileName = "./config.json";
+        public Bot Bot { get; set; }
+
+        public static Config GetConfig()
+        {
+            var configContent = File.ReadAllText(ConfigFileName);
+            return JsonConvert.DeserializeObject<Config>(configContent);
+        }
+    }
+
+    public class Bot
+    {
+        public string Token { get; set; }
+    }
+}
