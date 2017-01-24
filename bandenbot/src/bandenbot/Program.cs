5dca4ad34a479231acc8e8f72299374a70eabc5e
Lukas <lukasm@my.com>
2017-01-24 09:49:39 +0100

    Added the first bot functions

---
+++ bandenbot/src/bandenbot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bandenbot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stop = false;
+            var config = Config.GetConfig();
+            
+
+            //ToDo: Add Bot
+
+            while (!stop)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.S && key.Modifiers == ConsoleModifiers.Alt)
+                {
+                    stop = true;
+                }
+            }
+        }
+    }
+}
