b7273e3968dba11e4091cbcf9fd096cbd8d7f574
Lukas <lukasm@my.com>
2017-01-27 13:28:54 +0100

    added Jobs for Backup

---
+++ bandenbot/src/bandenbot/Jobs/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentScheduler;
+
+namespace bandenbot.Jobs
+{
+    public class Manager
+    {
+        public Manager()
+        {
+            JobManager.JobStart += JobManager_JobStart;
+            JobManager.JobEnd += JobManager_JobEnd;
+            JobManager.JobException += JobManager_JobException;
+        }
+
+        private void JobManager_JobException(JobExceptionInfo obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void JobManager_JobEnd(JobEndInfo obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void JobManager_JobStart(JobStartInfo obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void StartJobs() => JobManager.Initialize(new Registery());
+    }
+}
---
+++ bandenbot/src/bandenbot/Jobs/Registery.cs
@@ -0,0 +1,12 @@
+﻿using FluentScheduler;
+
+namespace bandenbot.Jobs
+{
+    public class Registery : Registry
+    {
+        public Registery()
+        {
+            Schedule<Backup>().ToRunNow().AndEvery(60).Minutes();
+        }
+    }
+}
